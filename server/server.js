fd65160f2c4c8c597d1f33d2d06fee965c3ffc17
gyulsangja <gojekal@naver.com>
2024-05-18 01:06:35 +0900

    v1.2

--- server/server.js
+++ server/server.js
@@ -20,8 +20,9 @@ app.listen(PORT, ()=>{
 
 app.use(express.static(path.join(__dirname, '/build')))
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/build', 'index.html'))
+app.get('/', (req, res)=>{
+    res.send('gyulstock')
+    // res.sendFile(path.join(__dirname, '/build', 'index.html'))
 })
 
 app.use('/login', require('./router/login.js'))
