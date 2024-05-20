56684490a5eed3261e5d725e61201cd78f342bb3
gyulsangja <gojekal@naver.com>
2024-05-20 17:58:27 +0900

    수정2

--- server/server.js
+++ server/server.js
@@ -21,8 +21,8 @@ app.listen(PORT, ()=>{
 app.use(express.static(path.join(__dirname, '/build')))
 
 app.get('/', (req, res)=>{
-    res.send('gyulstock')
-    // res.sendFile(path.join(__dirname, '/build', 'index.html'))
+    //res.send('gyulstock')
+    res.sendFile(path.join(__dirname, '/build', 'index.html'))
 })
 
 app.use('/login', require('./router/login.js'))
